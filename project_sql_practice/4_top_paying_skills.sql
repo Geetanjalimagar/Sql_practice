8ad3fc3d3b89c880af063d8017b49732c29b65dd
PratikShahiThakuri <35618890+PratikShahiThakuri@users.noreply.github.com>
2025-03-11 16:56:21 +1100

    initial commit

---
+++ project_sql_practice/4_top_paying_skills.sql
@@ -0,0 +1,62 @@
+SELECT 
+    skills,
+  ROUND(AVG(salary_year_avg), 0)as avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg is not null AND
+    job_work_from_home = TRUE
+GROUP BY  skills
+ORDER BY 
+avg_salary DESC
+limit 10
+
+
+/* 
+
+[
+  {
+    "skills": "pyspark",
+    "avg_salary": "208172"
+  },
+  {
+    "skills": "bitbucket",
+    "avg_salary": "189155"
+  },
+  {
+    "skills": "watson",
+    "avg_salary": "160515"
+  },
+  {
+    "skills": "couchbase",
+    "avg_salary": "160515"
+  },
+  {
+    "skills": "datarobot",
+    "avg_salary": "155486"
+  },
+  {
+    "skills": "gitlab",
+    "avg_salary": "154500"
+  },
+  {
+    "skills": "swift",
+    "avg_salary": "153750"
+  },
+  {
+    "skills": "jupyter",
+    "avg_salary": "152777"
+  },
+  {
+    "skills": "pandas",
+    "avg_salary": "151821"
+  },
+  {
+    "skills": "elasticsearch",
+    "avg_salary": "145000"
+  }
+]
+
+*/
